e450b6cb798c18b2ebfa9ccca422ccf07c5240b5
hausdinge <hausdinge@gmail.com>
2024-09-28 22:14:11 +0200

    split the hdmi serializer freq

--- MandelbulbVU9P/MandelbulbVU9P.srcs/sources_1/new/top_display.sv
+++ MandelbulbVU9P/MandelbulbVU9P.srcs/sources_1/new/top_display.sv
@@ -1,15 +1,32 @@
 `timescale 1ns / 1ps
 
+
+/*
+This is the top module for displaying the Mandelbulb.
+In order to display the Mandelbulb over HDMI, it was 
+required to reduce the frequency of the output signal
+from the HDMI-TMDS serializer. Instead of an output 
+frequency of 10x the pix_clk, we have now an output
+frequency of 5x the pix_clk by using double data rate 
+(DDR) signals.
+
+The output of this Module must be connected to some 
+output pins of the FPGA and the output pins should
+than be connected to some other device or circuid
+that implements the TMDS_ODDR_converter module.
+*/
+
+
 module top_display(
-input logic clk, // for now 100MHz clock
-input logic btnc, btnd, btnl, btnr, btnu, sw0, sw1,
-output logic hdmi_tx_clk_n,      // HDMI clock differential negative
-output logic hdmi_tx_clk_p,      // HDMI clock differential positive
-output logic [2:0] hdmi_tx_n,    // Three HDMI channels differential negative
-output logic [2:0] hdmi_tx_p     // Three HDMI channels differential positive
+input logic clk,                                      // 100 MHz
+input logic btnc, btnd, btnl, btnr, btnu, sw0, sw1,   // Buttons to move the Mandelbulb
+output logic o_pix_clk, o_pix_clk_5x,                 // Output clks needed for the TMDS_ODDR_converter module
+output logic TMDS_shift_red0, TMDS_shift_red1,        // Splitted colore data -> only 5x the pix_clk. This data
+output logic TMDS_shift_green0, TMDS_shift_green1,    // needs to be merged on the other device using TMDS_ODDR_converter module
+output logic TMDS_shift_blue0, TMDS_shift_blue1,      // or something else that implements an ODDR.
 );
   
-  ////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
                                // 640x480 1280x720
   localparam H_RES = 1280;     // 640     1280
@@ -22,23 +39,17 @@ output logic [2:0] hdmi_tx_p     // Three HDMI channels differential positive
   localparam V_FP = 5;         // 10      5
   localparam V_SYNC = 5;       // 2       5
   localparam V_BP = 20;        // 33      20
+  
+  // pix_clk (HD) = 1650x750x60 = 74.25 MHz
+  // clk_TDMS = 5 x pix_clk = 371.25 MHz
 
 ////////////////////////////////////////////////////////////////////////
 
   logic locked;
 
-  logic pix_clk; // 25MHz (640x480) or 74.25 MHz (1280x720)
-  logic clk_TMDS; // 250MHz (640x480) or 742.5 MHz (1280x720)
-  
-  logic [2:0] TMDSp, TMDSn;
-  logic TMDSp_clock, TMDSn_clock;
-  
-  assign hdmi_tx_p = TMDSp;
-  assign hdmi_tx_n = TMDSn;
-  assign hdmi_tx_clk_p = TMDSp_clock;
-  assign hdmi_tx_clk_n = TMDSn_clock;
-  
-  
+  logic pix_clk;    // 25MHz or 74.25 MHz
+  logic pix_clk_5x; // 125 MHz or 371.25 MHz
+   
   logic [12:0] CounterX = 0, CounterY = 0, x, y;
   logic hSync, vSync, DrawArea;
   
@@ -54,16 +65,17 @@ output logic [2:0] hdmi_tx_p     // Three HDMI channels differential positive
   assign y = CounterY;
   
   logic [7:0] red, green, blue;
-  logic [23:0] pix_color;
-  assign {red, green, blue} = pix_color;
   
 ////////////////////////////////////////////////////////////////////////
 
   // Draw something on the screen, simple test
   
-  /*always_ff @(posedge pix_clk) begin
+  /*logic [23:0] pix_color;
+  assign {red, green, blue} = pix_color;
+  
+  always_ff @(posedge pix_clk) begin
     if (x <= 400 && x >= 240 && y >= 200 && y <= 280) begin 
-      pix_color <= 24'h0000ff;
+      pix_color <= 24'h999999;
     end else begin
       pix_color <= 24'h000000;
     end
@@ -78,40 +90,83 @@ output logic [2:0] hdmi_tx_p     // Three HDMI channels differential positive
 
 ////////////////////////////////////////////////////////////////////////
   
-  // Uncomment when 480p (640x480) is used, comment out if 720p (1280x720) is used
+  // Uncomment when 480p is used, comment out if 720p is used
   /*clk_wiz_480p serial_and_pix_clk(
     .clk_out_25MHz(pix_clk),    
-    .clk_out_250MHz(clk_TMDS),      
+    .clk_out_125MHz(pix_clk_5x),      
     .clk(clk),    
     .locked(locked)
   );*/
   
-  // Uncomment when 720p (1280x720) is used, comment out if 480p (640x480) is used
+  // Uncomment when 720p is used, comment out if 480p is used
   clk_wiz_720p serial_and_pix_clk(
-    .clk_out_74MHz(pix_clk),     
-    .clk_out_742MHz(clk_TMDS),     
+    .clk_out_74_25MHz(pix_clk),     
+    .clk_out_371_25MHz(pix_clk_5x),     
     .clk(clk),
     .locked(locked)  
-  );     
-
+  );
 ////////////////////////////////////////////////////////////////////////
 
-  logic [3:0] TMDS_mod10 = 0;  // modulus 10 counter
-  logic [9:0] TMDS_shift_red = 0, TMDS_shift_green = 0, TMDS_shift_blue = 0;
-  logic TMDS_shift_load = 0;
+  // In this section the TMDS serializer with a 5x pix_clk frequency is implemented instead of 10x the pix_clk.
+  // The basic idea is that we serialize the every even and odd bit of the TMDS data seperately in the 
+  // falling and rising edge og the pix_clk_5x. Later the output of this module needs to be merged with for 
+  // example an oddr module (or the TMDS_ODDR_converter) to get a 10x pix_clk freq for HDMI input.
+
+  logic [2:0] TMDS_mod5 [0:1] = '{default:0};  // modulus 5 counter
+  logic [9:0] TMDS_shift_red [0:2] = '{default:0}, TMDS_shift_green [0:2] = '{default:0}, TMDS_shift_blue [0:2] = '{default:0};
+  logic [1:0] TMDS_shift_load = 0;
   
-  always_ff @(posedge clk_TMDS) begin
-    TMDS_shift_load <= (TMDS_mod10==4'd9);
-    TMDS_shift_red   <= TMDS_shift_load ? TMDS_red   : TMDS_shift_red  [9:1];
-    TMDS_shift_green <= TMDS_shift_load ? TMDS_green : TMDS_shift_green[9:1];
-    TMDS_shift_blue  <= TMDS_shift_load ? TMDS_blue  : TMDS_shift_blue [9:1];	
-    TMDS_mod10 <= (TMDS_mod10==4'd9) ? 4'd0 : TMDS_mod10+4'd1;
+  logic [1:0] shift_odd [0:4] = {1,2,2,2,2};
+  logic [9:0] TMDS_red_next, TMDS_green_next, TMDS_blue_next;
+  
+  // serialize every odd bit
+  always_ff @(posedge pix_clk_5x) begin
+    TMDS_shift_load[0] <= (TMDS_mod5[0]==3'd4);
+    TMDS_shift_red[0]   <= TMDS_shift_load[0] ? TMDS_red   : (TMDS_shift_red[0]  >> shift_odd[TMDS_mod5[0]]);
+    TMDS_shift_green[0] <= TMDS_shift_load[0] ? TMDS_green : (TMDS_shift_green[0] >> shift_odd[TMDS_mod5[0]]);
+    TMDS_shift_blue[0]  <= TMDS_shift_load[0] ? TMDS_blue  : (TMDS_shift_blue[0] >> shift_odd[TMDS_mod5[0]]);	
+    TMDS_mod5[0] <= (TMDS_mod5[0]==3'd4) ? 3'd0 : TMDS_mod5[0]+3'd1;
+    
+    TMDS_red_next <= TMDS_red;
+    TMDS_green_next <= TMDS_green;
+    TMDS_blue_next <= TMDS_blue;
   end
   
-  OBUFDS OBUFDS_red  (.I(TMDS_shift_red  [0]), .O(TMDSp[2]), .OB(TMDSn[2]));
-  OBUFDS OBUFDS_green(.I(TMDS_shift_green[0]), .O(TMDSp[1]), .OB(TMDSn[1]));
-  OBUFDS OBUFDS_blue (.I(TMDS_shift_blue [0]), .O(TMDSp[0]), .OB(TMDSn[0]));
-  OBUFDS OBUFDS_clock(.I(pix_clk), .O(TMDSp_clock), .OB(TMDSn_clock));
+  // serialize every even bit
+  always_ff @(negedge pix_clk_5x) begin
+    TMDS_shift_load[1] <= (TMDS_mod5[1]==3'd4);
+    TMDS_shift_red[1]   <= TMDS_shift_load[1] ? TMDS_red_next   : (TMDS_shift_red[0]  >> 1);
+    TMDS_shift_green[1] <= TMDS_shift_load[1] ? TMDS_green_next : (TMDS_shift_green[0] >> 1);
+    TMDS_shift_blue[1]  <= TMDS_shift_load[1] ? TMDS_blue_next  : (TMDS_shift_blue[0] >> 1);	
+    TMDS_mod5[1] <= (TMDS_mod5[1]==3'd4) ? 3'd0 : TMDS_mod5[1]+3'd1;
+  end
+  
+  // output all data
+  OBUF hdmi_pix_clk (.I(pix_clk), .O(o_pix_clk));
+  OBUF hdmi_serial_clk (.I(pix_clk_5x), .O(o_pix_clk_5x));
+  
+  assign TMDS_shift_red0   = TMDS_shift_red[0][0]; 
+  assign TMDS_shift_red1   = TMDS_shift_red[1][0];    
+  assign TMDS_shift_green0 = TMDS_shift_green[0][0];
+  assign TMDS_shift_green1 = TMDS_shift_green[1][0];
+  assign TMDS_shift_blue0  = TMDS_shift_blue[0][0]; 
+  assign TMDS_shift_blue1  = TMDS_shift_blue[1][0];  
+  
+  // Example instance
+  /*TMDS_ODDR_converter toc (
+    .pix_clk(pix_clk),
+    .pix_clk_5x(pix_clk_5x),
+    .TMDS_shift_red0(TMDS_shift_red[0][0]), 
+    .TMDS_shift_red1(TMDS_shift_red[1][0]),     
+    .TMDS_shift_green0(TMDS_shift_green[0][0]), 
+    .TMDS_shift_green1(TMDS_shift_green[1][0]), 
+    .TMDS_shift_blue0(TMDS_shift_blue[0][0]), 
+    .TMDS_shift_blue1(TMDS_shift_blue[1][0]),   
+    .TMDSp(TMDSp),
+    .TMDSn(TMDSn),
+    .TMDSp_clock(TMDSp_clock),
+    .TMDSn_clock(TMDSn_clock)                      
+  );*/
   
 ////////////////////////////////////////////////////////////////////////
   
@@ -154,11 +209,73 @@ endmodule
 
 ////////////////////////////////////////////////////////////////////////
 
+// This module could be implemented on an other device receiving the Data
+// from the VU9P and converts it back to an valid HDMI signal
+module TMDS_ODDR_converter (
+input logic pix_clk, pix_clk_5x,
+input logic TMDS_shift_red0, TMDS_shift_red1,
+input logic TMDS_shift_green0, TMDS_shift_green1,
+input logic TMDS_shift_blue0, TMDS_shift_blue1,
+output logic [2:0] TMDSp, TMDSn,
+output logic TMDSp_clock, TMDSn_clock
+);
+
+  logic TMDS_shift_red, TMDS_shift_green, TMDS_shift_blue;
+
+  ODDR #(
+    .DDR_CLK_EDGE("OPPOSITE_EDGE"), // "OPPOSITE_EDGE" or "SAME_EDGE"
+    .INIT(1'b0),    // Initial value of Q: 1'b0 or 1'b1
+    .SRTYPE("SYNC") // Set/Reset type: "SYNC" or "ASYNC"
+     ) ODDR_inst_red (
+    .Q(TMDS_shift_red),   // 1-bit DDR output
+    .C(pix_clk_5x),   // 1-bit clock input
+    .CE(1'b1), // 1-bit clock enable input
+    .D1(TMDS_shift_red0), // 1-bit data input (positive edge)
+    .D2(TMDS_shift_red1), // 1-bit data input (negative edge)
+    .R(1'b0),   // 1-bit reset
+    .S(1'b0)    // 1-bit set
+  );
+  
+  ODDR #(
+    .DDR_CLK_EDGE("OPPOSITE_EDGE"), // "OPPOSITE_EDGE" or "SAME_EDGE"
+    .INIT(1'b0),    // Initial value of Q: 1'b0 or 1'b1
+    .SRTYPE("SYNC") // Set/Reset type: "SYNC" or "ASYNC"
+     ) ODDR_inst_gren (
+    .Q(TMDS_shift_green),   // 1-bit DDR output
+    .C(pix_clk_5x),   // 1-bit clock input
+    .CE(1'b1), // 1-bit clock enable input
+    .D1(TMDS_shift_green0), // 1-bit data input (positive edge)
+    .D2(TMDS_shift_green1), // 1-bit data input (negative edge)
+    .R(1'b0),   // 1-bit reset
+    .S(1'b0)    // 1-bit set
+  );
+  
+  ODDR #(
+    .DDR_CLK_EDGE("OPPOSITE_EDGE"), // "OPPOSITE_EDGE" or "SAME_EDGE"
+    .INIT(1'b0),    // Initial value of Q: 1'b0 or 1'b1
+    .SRTYPE("SYNC") // Set/Reset type: "SYNC" or "ASYNC"
+     ) ODDR_inst_blue (
+    .Q(TMDS_shift_blue),   // 1-bit DDR output
+    .C(pix_clk_5x),   // 1-bit clock input
+    .CE(1'b1), // 1-bit clock enable input
+    .D1(TMDS_shift_blue0), // 1-bit data input (positive edge)
+    .D2(TMDS_shift_blue1), // 1-bit data input (negative edge)
+    .R(1'b0),   // 1-bit reset
+    .S(1'b0)    // 1-bit set
+  );
+  
+  OBUFDS OBUFDS_red  (.I(TMDS_shift_red), .O(TMDSp[2]), .OB(TMDSn[2]));
+  OBUFDS OBUFDS_green(.I(TMDS_shift_green), .O(TMDSp[1]), .OB(TMDSn[1]));
+  OBUFDS OBUFDS_blue (.I(TMDS_shift_blue), .O(TMDSp[0]), .OB(TMDSn[0]));
+  OBUFDS OBUFDS_clock(.I(pix_clk), .O(TMDSp_clock), .OB(TMDSn_clock));
+endmodule
+
+
 module TMDS_encoder(
-input clk,
-input [7:0] VD,  // video data (red, green or blue)
-input [1:0] CD,  // control data
-input VDE,       // video data enable, to choose between CD (when VDE=0) and VD (when VDE=1)
+input logic clk,
+input logic [7:0] VD,  // video data (red, green or blue)
+input logic [1:0] CD,  // control data
+input logic VDE,       // video data enable, to choose between CD (when VDE=0) and VD (when VDE=1)
 output logic [9:0] TMDS
 );
 
@@ -186,35 +303,3 @@ output logic [9:0] TMDS
     balance_acc <= VDE ? balance_acc_new : 4'h0;
   end
 endmodule
-
-////////////////////////////////////////////////////////////////////////
-
-//module TMDS_encoder_8bit(
-//	input logic clk,
-//	input logic [7:0] VD,  // video data (red, green or blue)
-//	input logic [1:0] CD,  // control data
-//	input logic VDE,  // video data enable, to choose between CD (when VDE=0) and VD (when VDE=1)
-//	output logic [7:0] TMDS
-//);
-
-//  logic [3:0] Nb1s = VD[0] + VD[1] + VD[2] + VD[3] + VD[4] + VD[5] + VD[6] + VD[7];
-//  logic XNOR = (Nb1s>4'd4) || (Nb1s==4'd4 && VD[0]==1'b0);
-//  logic [7:0] q_m = VD[7:0] ^ {8{XNOR}};  // Simplified the q_m calculation to fit 8-bit
-  
-//  logic [7:0] TMDS_data = q_m;  // Directly assign q_m to TMDS_data
-//  logic [7:0] TMDS_code = CD[1] ? (CD[0] ? 8'b10101011 : 8'b01010100) : (CD[0] ? 8'b00101011 : 8'b11010100);
-  
-//  always @(posedge clk) TMDS <= VDE ? TMDS_data : TMDS_code;
-
-//endmodule
-
-////////////////////////////////////////////////////////////////////////
-
-// VCU118 has High-Speed I/O (HSIO) banks with GTY transceivers, which are high-speed differential I/Os.
-// Use differential pairs for TMDS signals (which HDMI uses). These connections are done via PMOD or 
-// FMC connectors on the board.
-
-// TMDS Data0+ / Data0-: GTY transceiver pins (FMC_HPC_LA_0_P / FMC_HPC_LA_0_N)
-// TMDS Data1+ / Data1-: GTY transceiver pins (FMC_HPC_LA_1_P / FMC_HPC_LA_1_N)
-// TMDS Data2+ / Data2-: GTY transceiver pins (FMC_HPC_LA_2_P / FMC_HPC_LA_2_N)
-// TMDS CLK+ / CLK-: GTY transceiver pins (FMC_HPC_LA_3_P / FMC_HPC_LA_3_N)
